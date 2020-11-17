98a851961124d51b1711a107224cbb4f0d9b3c28
VuongTuanSon <tuansonvuong@gmail.com>
2020-11-17 20:35:56 +0700

    fix products

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ import Footer from './footerComponents/footer';
 import HomePage from './pages/homePage';
 import SlideShow from './pages/slideShow';
 import Products from './pages/products';
-import Products2 from './pages/products2';
-import Products3 from './pages/products3';
-import Products4 from './pages/products4';
-import Products5 from './pages/products5';
 import './assets/default.css'
 
 function App() {
@@ -25,10 +21,6 @@ function App() {
               <Route exact path='/' component={SlideShow} />
               <Route exact path='/' component={HomePage} />
               <Route exact path='/Products' component={Products} />
-              <Route exact path='/Products2' component={Products2} />
-              <Route exact path='/Products3' component={Products3} />
-              <Route exact path='/Products4' component={Products4} />
-              <Route exact path='/Products5' component={Products5} />
               <Footer />
     </div>
     </Router>
